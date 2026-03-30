f6ef25f06a80d9b3f8458060341b75abe0a17716
thaiphucthanh <108589032+thaiphucthanh@users.noreply.github.com>
2026-03-30 21:52:18 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# conghoclieumoncaulong
-Đây là cổng học liệu dành cho học sinh lớp 10 học môn Cầu Lông
+# portalsport
---
+++ script.js
@@ -0,0 +1,11 @@
+$(function(){ 							   
+
+	// radiys box
+	$('.header').css({"border-radius": "10px 0 10px 0", "-moz-border-radius":"10px 0 10px 0", "-webkit-border-radius":"10px 0 10px 0"});
+	$('.hbg').css({"border-radius": "0 20px 20px 0", "-moz-border-radius":"0 20px 20px 0", "-webkit-border-radius":"0 20px 20px 0"});
+	$('.pagenavi a, .pagenavi .current').css({"border-radius": "5px", "-moz-border-radius":"5px", "-webkit-border-radius":"5px"});
+	$('.mainbar .spec a.rm').css({"border-radius": "10px", "-moz-border-radius":"10px", "-webkit-border-radius":"10px"});
+	$('.fbg').css({"border-radius": "20px 0 0 0", "-moz-border-radius":"20px 0 0 0", "-webkit-border-radius":"20px 0 0 0"});
+	$('.content').css({"border-radius": "0 20px 0 20px", "-moz-border-radius":"0 20px 0 20px", "-webkit-border-radius":"0 20px 0 20px"});
+	
+});	
